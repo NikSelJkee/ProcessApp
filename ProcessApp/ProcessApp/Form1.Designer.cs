f5af80a40be26f324817b00864f6934f2fe3cdf9
Nikita <37782020+NikSelJkee@users.noreply.github.com>
2019-05-30 11:30:26 +0300

    Add files via upload

---
+++ ProcessApp/ProcessApp/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ProcessApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxProcess = new System.Windows.Forms.ListBox();
+            this.buttonEnable = new System.Windows.Forms.Button();
+            this.buttonDisable = new System.Windows.Forms.Button();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxProcess
+            // 
+            this.listBoxProcess.FormattingEnabled = true;
+            this.listBoxProcess.Location = new System.Drawing.Point(12, 12);
+            this.listBoxProcess.Name = "listBoxProcess";
+            this.listBoxProcess.Size = new System.Drawing.Size(376, 186);
+            this.listBoxProcess.TabIndex = 0;
+            // 
+            // buttonEnable
+            // 
+            this.buttonEnable.Location = new System.Drawing.Point(12, 223);
+            this.buttonEnable.Name = "buttonEnable";
+            this.buttonEnable.Size = new System.Drawing.Size(75, 23);
+            this.buttonEnable.TabIndex = 1;
+            this.buttonEnable.Text = "Включить";
+            this.buttonEnable.UseVisualStyleBackColor = true;
+            this.buttonEnable.Click += new System.EventHandler(this.ButtonEnable_Click);
+            // 
+            // buttonDisable
+            // 
+            this.buttonDisable.Location = new System.Drawing.Point(160, 223);
+            this.buttonDisable.Name = "buttonDisable";
+            this.buttonDisable.Size = new System.Drawing.Size(75, 23);
+            this.buttonDisable.TabIndex = 2;
+            this.buttonDisable.Text = "Отключить";
+            this.buttonDisable.UseVisualStyleBackColor = true;
+            this.buttonDisable.Click += new System.EventHandler(this.ButtonDisable_Click);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(313, 223);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(75, 23);
+            this.buttonRefresh.TabIndex = 3;
+            this.buttonRefresh.Text = "Обновить";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.ButtonRefresh_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 267);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.buttonDisable);
+            this.Controls.Add(this.buttonEnable);
+            this.Controls.Add(this.listBoxProcess);
+            this.Name = "MainForm";
+            this.Text = "Process Manager";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxProcess;
+        private System.Windows.Forms.Button buttonEnable;
+        private System.Windows.Forms.Button buttonDisable;
+        private System.Windows.Forms.Button buttonRefresh;
+    }
+}
+
---
+++ ProcessApp/ProcessApp/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceProcess;
+
+namespace ProcessApp
+{
+    public partial class MainForm : Form
+    {
+        ServiceController[] services;
+        public MainForm()
+        {
+            InitializeComponent();
+            
+
+            services = ServiceController.GetServices();
+
+            foreach (var item in services)
+                listBoxProcess.Items.Add("Process: " + item.ServiceName + " - Status: " + item.Status);
+        }
+
+        private void ButtonEnable_Click(object sender, EventArgs e)
+        {
+            if (listBoxProcess.SelectedItem == null)
+                return;
+            ServiceController ser = services[listBoxProcess.SelectedIndex];
+            if (ser.Status == ServiceControllerStatus.Running)
+            {
+                MessageBox.Show("Процесс уже запущен!");
+                return;
+            }
+            ser.Start();
+            listBoxProcess.Items.Clear();
+            services = ServiceController.GetServices();
+            foreach (var item in services)
+                listBoxProcess.Items.Add("Process: " + item.ServiceName + " - Status: " + item.Status);
+            MessageBox.Show("Процесс запущен!");
+        }
+
+        private void ButtonDisable_Click(object sender, EventArgs e)
+        {
+            if (listBoxProcess.SelectedItem == null)
+                return;
+            ServiceController ser = services[listBoxProcess.SelectedIndex];
+            if (ser.Status == ServiceControllerStatus.Stopped)
+            {
+                MessageBox.Show("Процесс уже остановлен!");
+                return;
+            }
+            ser.Stop();
+            listBoxProcess.Items.Clear();
+            services = ServiceController.GetServices();
+            foreach (var item in services)
+                listBoxProcess.Items.Add("Process: " + item.ServiceName + " - Status: " + item.Status);
+            MessageBox.Show("Процесс остановлен!");
+        }
+
+        private void ButtonRefresh_Click(object sender, EventArgs e)
+        {
+            services = ServiceController.GetServices();
+            foreach (var item in services)
+                listBoxProcess.Items.Add("Process: " + item.ServiceName + " - Status: " + item.Status);
+        }
+    }
+}
